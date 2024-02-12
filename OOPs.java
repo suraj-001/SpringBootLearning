d14ca600b74f4ace641392231a5936d5d2b53aff
Suraj Agarwal <surajagarwal@fico.com>
2024-02-12 23:58:50 +0530

    date:12/2/24

--- OOPs.java
+++ OOPs.java
@@ -3,9 +3,19 @@ class Calculator{
     int a;
     public int add(int a,int b)
     {
-        System.out.println("Hmmm ! let me these two numbers ");
+        System.out.println("Hmmm ! let me sum these two numbers ");
         return a+b;
 
+    }
+    public int add(int a,int b,int c){
+        System.out.println("Oho! so now you want sum of three numbers.okay so sum is:-");
+        return a+b+c;
+    }
+}
+class doSomething
+{
+    public void playMusic(){
+        System.out.println("Music Playing..0 1 2");
     }
 }
 public class OOPs {
@@ -16,5 +26,10 @@ public class OOPs {
         Calculator calc = new Calculator();
         int result=calc.add(num1,num2);   
         System.out.println("sum of two numbers is " + result); 
+        System.out.println("Give me some of three numbers");
+        System.out.println(calc.add(2,3,4));
+        doSomething play = new doSomething();
+        play.playMusic();
+
     }
 }
